b732c6355049acd41b99aad44317feed3d9a57e5
KYLE S <79766794+418Gaming@users.noreply.github.com>
2021-12-09 11:26:14 -0500

    Edited Start locations

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config = {}
-Config.StartingApartment = true -- Enable/disable starting apartments (make sure to set default spawn coords)
-Config.Interior = vector3(-814.89, 181.95, 76.85) -- Interior to load where characters are previewed
+Config.StartingApartment = false -- Enable/disable starting apartments (make sure to set default spawn coords)
+Config.Interior = vector3(-703.94, -152.55, 37.42) -- Interior to load where characters are previewed
 Config.DefaultSpawn = vector3(-1035.71, -2731.87, 12.86) -- Default spawn coords if you have start apartments disabled
-Config.PedCoords = vector4(-813.97, 176.22, 76.74, -7.5) -- Create preview ped at these coordinates
-Config.HiddenCoords = vector4(-812.23, 182.54, 76.74, 156.5) -- Hides your actual ped while you are in selection
-Config.CamCoords = vector4(-813.46, 178.95, 76.85, 174.5) -- Camera coordinates for character preview screen
+Config.PedCoords = vector4(-705.36, -151.88, 37.42, 258.06) -- Create preview ped at these coordinates
+Config.HiddenCoords = vector4(-701.57, -151.94, 37.42, 90.96) -- Hides your actual ped while you are in selection
+Config.CamCoords = vector4(-702.79, -152.09, 37.42, 86.7) -- Camera coordinates for character preview screen
